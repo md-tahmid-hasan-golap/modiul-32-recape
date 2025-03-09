1051f692cc9deba42ebe7207046381e25a0f2c25
golap-raj <golapraj47@gmail.com>
2025-03-09 22:37:13 +0600

    add trul false

---
+++ index.js
@@ -0,0 +1,208 @@
+// data type 
+
+
+// let name = "Golap";
+// console.log(name);
+
+// let newName = name;
+
+// console.log(newName)
+
+// name = "Tahmid";
+// console.log(newName)
+
+// let person = {Name : "Golap"};
+
+// let NewName = person;
+// person.Name = "tahmid   "
+// console.log(NewName)
+
+// let myName = "Golap Raj"
+
+// let newName =  myName;
+// myName = "Tahmid Hasan"
+// console.log(newName);
+
+
+// let person ={Name : "Golap"};
+
+// let newPerson = person;
+// person.Name= "tahmid"
+// console.log(newPerson)
+
+
+// java script equal 
+
+
+// const num1 = 10;
+
+// const num2 = "10";
+
+// // ata sudhu matro man chack kore
+
+// if(num1 == num2){
+//     console.log("this is a equal")
+// }
+// else{
+//     console.log("this is a not equal")
+// }
+
+
+// const num1 = 10;
+
+// const num2 = "10";
+
+// const num3 = parseInt(num2)
+
+// // ata  man & type o chack kore
+
+// if(num1 === num3){
+//     console.log("this is a equal")
+// }
+// else{
+//     console.log("this is a not equal")
+// }
+
+
+// const num1 = 10;
+
+// const num2 = "10";
+
+// console.log(num1 + num2)
+
+
+// ata  man & type o chack kore
+
+// if(num1 == num2){
+//     console.log("this is a equal")
+// }
+// else{
+//     console.log("this is a not equal")
+// }
+
+
+// if(5*"2" === 10){
+//     console.log("OKy")
+// }
+// else{
+//     "noo"
+// }
+
+
+//  null - undefined - java script
+
+
+
+// null hole jar moddhe kichu ache ,& undefined holo jar moddhe kichu nai
+
+// const name = undefined;
+// console.log(name);
+
+// const name1 = null;
+// console.log(name1)
+
+
+// let name;
+// console.log(name)
+
+
+// rigth no undifint
+
+// function sum (num1, num2){
+//    return num1 + num2
+// }
+// const output = sum(10,30);
+// console.log(output)
+
+// undefined
+
+// function sum (num1, num2){
+//    return
+// }
+// const output = sum(10,30);
+// console.log(output)
+
+// undefined
+
+// function sum (num1, num2){
+//    console.log(num1 + num2)
+// }
+// const output = sum(10,30);
+// console.log(output)
+
+
+// true false java script
+
+
+
+// this is a truthy value
+
+// const name = " "
+// // console.log(name)
+// if(name){
+//     console.log("This Is A truthy Value")
+// }
+// else{
+//     "This Is A falsy Value"
+// }
+
+// this is a falsy value
+
+// const name = ""
+// // console.log(name)
+// if(name){
+//     console.log("This Is A truthy Value")
+// }
+// else{
+//    console.log( "This Is A falsy Value")
+// }
+
+// // this is a truthy value
+
+// const name = "golap"
+// // console.log(name)
+// if(name){
+//     console.log("This Is A truthy Value")
+// }
+// else{
+//     "This Is A falsy Value"
+// }
+// this is a falsy value
+
+// const name = false
+// // console.log(name)
+// if(name){
+//     console.log("This Is A truthy Value")
+// }
+// else{
+//     "This Is A falsy Value"
+// }
+
+//  truthy value a gula 1-100
+// const name = 1
+// // console.log(name)
+// if(name){
+//     console.log("This Is A truthy Value")
+// }
+// else{
+//     "This Is A falsy Value"
+// }
+
+// 0 thakle falsy value
+
+// const name = 1
+// // console.log(name)
+// if(name){
+//     console.log("This Is A truthy Value")
+// }
+// else{
+//     "This Is A falsy Value"
+// }
+
+
+// const name ="golap";
+// console.log(!name)
+
+
+// const name ="";
+// console.log(!name)
